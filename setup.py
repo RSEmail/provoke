aa7ad65078bc740a33a2dd3903fa8a6e0525bcec
Ian Good <ian.good@rackspace.com>
2015-02-16 13:46:29 -0500

    set version 0.3.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 
 setup(name='provoke',
-      version='0.3.1',
+      version='0.3.2',
       author='Ian Good',
       author_email='ian.good@rackspace.com',
       description='Lightweight, asynchronous function execution in Python '
